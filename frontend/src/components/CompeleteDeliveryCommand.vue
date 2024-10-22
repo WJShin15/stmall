c2064070222a9c708e17a52ef57d6493b6ed26ff
WJShin15 <swjfberd@gmail.com>
2024-10-22 15:33:37 +0900

    commit

---
+++ frontend/src/components/CompeleteDeliveryCommand.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-card outlined>
+        <v-card-title>
+            CompeleteDelivery
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="primary"
+                    text
+                    @click="compeleteDelivery"
+            >
+                CompeleteDelivery
+            </v-btn>
+            
+            <v-btn
+                    color="primary"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'CompeleteDeliveryCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+        },
+        watch: {
+        },
+        methods: {
+            compeleteDelivery() {
+                this.$emit('compeleteDelivery', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
---
+++ frontend/src/components/CompeleteReturnCommand.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-card outlined>
+        <v-card-title>
+            CompeleteReturn
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="primary"
+                    text
+                    @click="compeleteReturn"
+            >
+                CompeleteReturn
+            </v-btn>
+            
+            <v-btn
+                    color="primary"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'CompeleteReturnCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+        },
+        watch: {
+        },
+        methods: {
+            compeleteReturn() {
+                this.$emit('compeleteReturn', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
